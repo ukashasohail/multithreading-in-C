8b7952cf3472947b0319e5f588b230b8c41f0d76
ukashasohail <ukasha909@outlook.com>
2020-08-12 13:13:04 +0500

    done

--- code2.c
+++ code2.c
@@ -2,11 +2,16 @@
 #include <pthread.h>
 #include <unistd.h>
 
+// void pointers are generic so any datatype can be passed and returned
 void* my_turn(){
+
+    int *iptr = (int *)arg;
+
     for (int i = 0; i < 8; i++)
     {
         sleep(1);
         printf("my turn %d\n",i);
+        (*iptr)++;
     }
 }
 
@@ -22,9 +27,11 @@ void your_turn(){
 int main(){
 
     pthread_t newthread;
+    int v=5;
 
-    pthread_create(&newthread, NULL, my_turn, NULL);
+    pthread_create(&newthread, NULL, my_turn, &v);
     // my_turn();
     your_turn();
     pthread_join(newthread, NULL); // didnt do &newthread as it was giving an error
+    printf("threads are done: v=%d\n",v);
 }
